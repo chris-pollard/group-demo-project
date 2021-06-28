36966e41d748ea4b64de4eddedeef2d1f9af50e8
chris-pollard <therealchrispollard@gmail.com>
2021-06-28 15:39:47 +1000

    testing a comment in server file

--- server.js
+++ server.js
@@ -5,3 +5,6 @@ const port = 4567
 app.get('/', (req, res) => res.send('hello world'))
 
 app.listen(port, () => console.log(`listening on port: ${port}`))
+
+// I am making a comment here to test (made by crispy)
+
